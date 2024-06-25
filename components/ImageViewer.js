f53e550a985e22023633098f1aa883706f3653b7
Terry TianliangShi <tianliangshi@shinetechsoftware.com>
2024-06-25 23:55:44 +0800

    add options buttons

--- components/ImageViewer.js
+++ components/ImageViewer.js
@@ -1,8 +1,9 @@
 import { StyleSheet, Image } from "react-native";
 
-export default function ImageView ({placeholderImageSource}) {
+export default function ImageView ({placeholderImageSource, selectedImage}) {
+    const imageSource = selectedImage ? {uri: selectedImage} : placeholderImageSource;
     return (
-        <Image source={placeholderImageSource} style={styles.image}></Image>
+        <Image source={imageSource} style={styles.image}></Image>
     )
 }
 
